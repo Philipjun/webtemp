c7d3865588e894f8a72989a3ff4b6e6f924e0755
Philip Jun <inbox@philipjun.com>
2014-06-26 19:47:41 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Philipjun::Application.config.secret_key_base = '1226f5165428d89fbf90cfaf4a6732b7d846833e0df643e0ee8f30e646442e885db07589c103c35d28222769533a44fedadfa690851ba6c67e990f2154abd1ae'
