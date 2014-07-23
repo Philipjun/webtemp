1c5b220a89e49080f40b2f23eb50dd9188911bde
Philip Jun <inbox@philipjun.com>
2014-07-23 12:30:57 -0400

    added strong params to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,17 @@
 class PostsController < ApplicationController
 
-def new
+  def new
 
-end
+  end
 
-def create
-  render plain: params[:posts].inspect
-end
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+    redirect_to @post
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
 
 end
