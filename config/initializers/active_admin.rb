e06ed5777fb55d47a2a90a630ef31d24de608b5c
Philip Jun <inbox@philipjun.com>
2014-07-23 23:00:28 -0400

    changed admin css and js into new directory

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Philipjun"
+  config.site_title = "Philjun"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -163,6 +163,7 @@ ActiveAdmin.setup do |config|
   #
   # To load a stylesheet:
   #   config.register_stylesheet 'my_stylesheet.css'
+    config.register_stylesheet 'admin/active_admin.css'
   #
   # You can provide an options hash for more control, which is passed along to stylesheet_link_tag():
   #   config.register_stylesheet 'my_print_stylesheet.css', :media => :print
@@ -170,6 +171,7 @@ ActiveAdmin.setup do |config|
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
 
+    config.register_javascript 'admin/active_admin.js'
 
   # == CSV options
   #
