11480e89827fb5ba58defd58704285ae2b2fc083
Philip Jun <inbox@philipjun.com>
2014-07-25 00:28:14 -0400

    updated overlay scroll and blog seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,54 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Post.create(id: 1, title: "Test", text: "Test post", created_at: "2014-07-25", image: "monogram.jpg")
+
+
+# Post.create(id: 2, title: "Last Day at Bitmaker Labs-Demo Day", text: "Wow it’s crazy how fast the 9 weeks went by! We had an internal Bitmaker Labs Demo Day on Friday to showcase all the final projects that the students worked on during the last 2 weeks of the course. It was amazing to see all the unique and creative web apps that my fellow classmates have built! We have definitely come a long way since the first week of class, where we were learning about the basics.", created_at: "2014-05-24", image: "monogram.jpg")
+
+Post.create(id: 1, title: "Day 1 at Bitmaker Labs", text: "My first day at Bitmaker Labs was definitely a memorable day! It first got started off by an introduction of all the various staff members, including the co-founders and the teachers with some interesting things about them, such as their favourite teacher. We were then split into random teams and was asked to make a creative presentation that introduced all the members of our team. That was definitely a great ice breaker to get to know everybody, and it was pretty interesting to have such a diverse group of people in the class ranging from professionals wanting to change careers to recent student graduates. 
+
+Shortly after the introductions and ice breaker games, we then got started on Git, Unix, and Github. It was a great refresher to the prep materials and was fun to work on an exercise based on the movie Inception, except we had to move files and folders through multiple levels. I was able to get everything setup on Github properly, pushed my assignment correctly and submitted it for that day!", created_at: "2014-03-29")
+
+Post.create(id: 2, title: "Day 2 at Bitmaker Labs", text: "Today things got a lot more interesting as we went straight into Ruby fundamentals! First we did a few drills using irb on Terminal to do basic things, such as using numbers, strings, booleans, and variables.
+
+We then progressed into using IF,ELSE and ELSIF,  statements -brings back DWA memories for those Ivey grads.
+
+The first assignment was to create a file that is based on a popular coding challenge called Fizz Buzz, except we had to output Bit and Bitmaker. I thought this assignment was fairly simple as it was just review of what was in the prep work before, but I did learn that it is very important to put items in the priority that they should be called, otherwise the code will just stop -key learning point for today.", created_at: "2014-03-29")
+
+Post.create(id: 3, title: "Day 3", text: "Dedicating 9 weeks of your time and investing more than $9k for the tuition is not an easy decision to make, I know I was pondering over the decision very heavily before I finally committed.
+
+After only being in the class for 2 days, I already felt this would be a life-changing experience. To be honest I was a bit skeptical at first, which I assume many of those reading my blog can relate to as well – can you really learn to program in 9 weeks and is this a legit class? My answer to both those questions > YES!
+
+Day 3 got started off by waffles in the morning, which occurs every Wednesday and 99% of my classmates were all there early to indulge the wonderful waffles made by Will (one of the co-founders) and to code before class. Class officially begins at 10am but most people, including myself get there for 9am to start off the day.
+
+Today’s lecture was to continue working on Ruby fundamentals, focusing on creating methods, arrays, and hashes. It was mainly review of the prep work but it really reinforced the major concepts that we will be using later on.
+
+Another great perk is the group yoga on Wednesdays after the lecture, which usually ends around noon. After grabbing a quick lunch, we set off to Yoga and I realized that I needed to work more on my lower back at the gym haha. I thought this yoga session was going to be intense like the Exhale class I went to in Santa Monica-which exhausted me out 20 mins into the 1hr class. But it ended up being a great balance between being open to beginners (I only did yoga a few times) and pushing yourself to become more flexible.
+
+After the great yoga session, we all headed back to class and continued to code until around 6pm – when people start to head out and continue coding at home.
+
+Another successful day!", created_at: "2014-03-29")
+
+Post.create(id: 4, title: "Day 4", text: "Wow this week is already almost over, time flies! So far I’ve been having an amazing time at Bitmaker Labs! I have no regrets and love coming to class everyday, knowing I will always be learning something new.
+
+Today, I was there at 9am for the coding drills – which happens every Tuesday and Thursday by one of the instructors. It is great to review material from class the day before and really kicks off the day!
+
+The lecture today was on Object Oriented Programming – which is the trickiest section of Ruby to learn as a beginner. This where will learn about classes, inheritance, modules & namespacing.
+
+I was faced with one of the toughest coding challenges I experienced in the course so far – I did not even know how to get started, which frustrated me A LOT. Luckily, I was not the only one and many of my classmates were in the same position – which eased my frustrations.
+
+So the coding challenge/assignment today was to create a Robot that will be moving across a 5×5 grid and there was a controller that would tell where the Robot should go and in which direction. I was really lost in terms of how to structure the code. Luckily, Chris – one of the morning instructors, gave us a lecture on how to approach the problem. The problem became very clear after he walked us through the steps in structuring the code that allowed me to no longer be frustrated. An analogy to HBA grads is the first DWA case we got…I did not know how to even get started on it and it turned out it to be a case near the end of the course – which became much more achievable after building my knowledge. I like to think of the Robot assignment the same way.
+
+Today was especially interesting because it was Think Thursday! Think Thursday is a weekly speaker event that allows members of the tech community to attend speaking events by various individuals and learn from their experiences. Another perk of Bitmaker Labs! I encourage anybody interested to signup on Meetups, you do not have to be a Bitmaker Labs student to attend and most are free!
+
+Stephen and Matt, Founders of Filament Labs – a leading digital agency in Toronto, came to speak on failures. Which is interesting because many successful entrepreneurs like to provide lectures on the successes, not necessarily on the failure they encountered to become successful. Overall, it was a great event to learn from their experiences. The event was named “We F#cked Up!” to really illustrate the focus on failures.", created_at: "2014-03-29")
+
+Post.create(id: 5, title: "Day 5", text: "It is the end of the first week already and it’s been a crazy ride! I have learned more about ruby this week than i did when I tried to teach myself through various online tutorials, such as Codecademy, and Michael Hartyl’s Ruby tutorial.
+
+Today’s lecture was to build on using Object oriented programming and learning to think as a developer. This meant focusing on user stories, structuring out programs and actually writing the code. In my opinion, today was the most important lecture of the week because it focuses on the building blocks to become a successful/stronger programmer.
+
+Our exercise today was to build our own Customer Relationship Management (CRM) program and being able to add, remove, list, and update customer contacts. Fun project! I am currently still fine tuning the code as I write this post but will be updating the code later for everyone to see!", created_at: "2014-03-30")
+
+Post.create(id: 6, title: "Day 7", text: "Today was a fun today to learn how to make our own web servers. Learned about the basics of creating a HTML response.
+
+The weather was amazing today and signs of Winter being over are starting to show. I went to grab a quick coffee break with an IT recruiter to discuss a potential opportunity. Funny enough, actually met a recent Ivey Mba grad that was a table beside us and overheard us talking, small world!", created_at: "2014-03-30")
